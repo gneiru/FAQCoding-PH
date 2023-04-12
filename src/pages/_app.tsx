45effe49a1765e6c1db8e66cd0a839fa2ba26a2c
gneiru <noelrohi59@gmail.com>
2023-04-12 21:41:36 +0800

    + : clerk set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,16 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { NavBar } from "~/components/Navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <NavBar />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
